759ff57939000576fc7d33be3cbbe5a46372399b
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2024-02-03 21:32:30 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,25 @@
 from pymongo import MongoClient
 import os
+from datetime import datetime
+import pytz
 
 # Connect to MongoDB
 mongo_client = MongoClient(os.getenv('mongodb'))
 db = mongo_client['Sites']
-log = db['redirect']
+collection = db['redirect']
+
+def log(request, url):
+    collection.insert_one({
+        "url_set": url,
+        "by_ip": request.headers.get('X-Forwarded-For', request.remote_addr),
+        "user": request.headers.get('User-Agent', 'N/A'), 
+        "time": datetime.now(pytz.timezone("Asia/Kolkata")).strftime("%Y-%m-%d %H:%M:%S")
+    })
+
+def access(request, url):
+    collection.insert_one({
+        "redirect_url": url,
+        "ip": request.headers.get('X-Forwarded-For', request.remote_addr),
+        "redirected_user": request.headers.get('User-Agent', 'N/A'), 
+        "time": datetime.now(pytz.timezone("Asia/Kolkata")).strftime("%Y-%m-%d %H:%M:%S")
+    })
